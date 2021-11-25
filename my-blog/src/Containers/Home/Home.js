40f5d91531c7e4ae65e4bfaff321062fccff323f
g-veronika <g.veronika2021@gmail.com>
2021-11-25 18:04:28 +0100

    show up articles

--- my-blog/src/Containers/Home/Home.js
+++ my-blog/src/Containers/Home/Home.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import './Home.css';
 import Card from '../../Components/Card/Card'
+import { useSelector, useDispatch, } from 'react-redux'; 
+import { useState, useEffect } from 'react';
+import { getArticles } from '../../redux/articles/articleReducer';
+import {v4 as uuidv4} from 'uuid';
 
 export default function Home() {
+
+    const {articles} = useSelector(state => ({
+        ...state.articleReducer
+    }))
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(articles.length === 0) {
+            dispatch(getArticles());
+        }
+    }, [])
+
     return (
         <>
             <h1 className="home-title">Tous les articles</h1>
             <div className="container-cards">
-                <Card>
-                    <h2>Hello Card</h2>
-                </Card>
+                {articles.map(item => {
+                    return (
+                        <Card key={uuidv4()}>
+                            <h2>{item.title}</h2>
+                            <a href="#">Lire l'article</a>
+                        </Card>
+                    )
+                })}
             </div>
         </>
     )
