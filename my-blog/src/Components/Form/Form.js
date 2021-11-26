ba4c50afbee0e55bf7cc109c44c17d40e23e562f
g-veronika <g.veronika2021@gmail.com>
2021-11-26 18:03:32 +0100

    data binding

--- my-blog/src/Components/Form/Form.js
+++ my-blog/src/Components/Form/Form.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
 import './Form.css';
 
 export default function Form() {
+
+    const [article, setArticle] = useState({
+        title: "",
+        body: ""
+    })
+
+    const dispatch = useDispatch();  
+
+
+    const handleForm = e => {
+        e.preventDefault();
+    }
+
+    const handleInput = e => {
+
+        console.log("HELLO")
+
+    }
+
+
     return (
+
         <>
             <h1 className="title-form">Ecrivez un article</h1>
-            <form className="container-form">
+            <form onSubmit={handleForm} className="container-form">
                 <label htmlFor="title">Titre</label>
-                <input type="text" id="title" placeholder="Entrez votre nom"/>
+                <input 
+                onChange={handleInput}
+                value={article.title}
+                type="text" 
+                id="title" 
+                placeholder="Entrez votre nom"/>
 
                 <label htmlFor="article">Votre article</label>
-                <textarea id="article" placeholder="Votre article"></textarea>
+                <textarea 
+                onChange={handleInput}
+                value={article.body}
+                id="article" 
+                placeholder="Votre article"></textarea>
 
                 <button>Envoyez l'article</button>
 
