aaef13d4519b9cadb3090826c37b4682135b3fa4
g-veronika <g.veronika2021@gmail.com>
2021-11-26 16:10:39 +0100

    creating form

---
+++ my-blog/src/Components/Form/Form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Form() {
+    return (
+        <>
+            <h1 className="title-form">Ecrivez un article</h1>
+            <form className="container-form">
+                <label htmlFor="title">Titre</label>
+                <input type="text" id="title" placeholder="Entrez votre nom"/>
+
+                <label htmlFor="article">Votre article</label>
+                <textarea id="article" placeholder="Votre article"></textarea>
+
+                <button>Envoyez l'article</button>
+
+            </form>
+        </>
+    )
+}
