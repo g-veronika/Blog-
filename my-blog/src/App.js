e52d727d9db069965d2405b42f604d672cf8b22b
g-veronika <g.veronika2021@gmail.com>
2021-11-26 15:08:23 +0100

    creating router

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,12 +1,23 @@
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Containers/Home/Home';
+import {Routes, Route} from 'react-router-dom';
+import AddArticle from './Containers/AddArticle/AddArticle';
+import Contact from './Containers/Contact/Contact';
+import Article from './Containers/Article/Article';
 
 function App() {
   return (
-    <div className="App">
+    <> 
       <Navbar />
-      <Home />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/ecrire" element={<AddArticle />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/article" element={<Article />}/>
+
+      </Routes>
+    </>
   );
 }
 
--- my-blog/src/Components/Navbar/Navbar.js
+++ my-blog/src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react'
-import './Navbar.css'
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
 
@@ -32,9 +33,19 @@ export default function Navbar() {
         <nav>
             {(toggleMenu || largeur > 500) && (
                 <ul className="liste">
-                    <li className="items">Accueil</li>
-                    <li className="items">Ecrire</li>
-                    <li className="items">Contact</li>
+
+                    <li className="items">
+                        <Link to="/">Accueil</Link>
+                    </li>
+
+                    <li className="items">
+                        <Link to="/ecrire">Ecrire</Link>
+                    </li>
+
+                    <li className="items">
+                        <Link to="/contact">Contact</Link>
+                    </li>
+
                 </ul>
             )}
             <button onClick={toggleNav} className="btn">CLIQUE ICI</button>
